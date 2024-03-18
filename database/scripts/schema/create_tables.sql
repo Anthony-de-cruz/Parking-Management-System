473dffd30def84ad8c2aaaff9feb0387d92c9991
Anthony de Cruz <anthonydecruz@proton.me>
2024-03-18 10:34:19 +0000

    Made initial db schema

---
+++ database/scripts/schema/create_tables.sql
@@ -0,0 +1,38 @@
+-- REBUILD Script
+-- NUCLEAR BOMB
+DROP TABLE IF EXISTS "ParkingSpace";
+DROP TABLE IF EXISTS "Carpark";
+DROP TABLE IF EXISTS "Vehicle";
+DROP TABLE IF EXISTS "User";
+
+CREATE Table "User" (
+    id INTEGER,
+    username VARCHAR(20) NOT NULL,
+    password VARCHAR(20) NOT NULL,
+    email VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE Table "Vehicle" (
+    registration VARCHAR(7),
+    id INTEGER NOT NULL,
+    PRIMARY KEY (registration),
+    FOREIGN KEY (id) REFERENCES "User" (id)
+);
+
+CREATE Table "Carpark" (
+    carparkID INTEGER,
+    name VARCHAR(20) NOT NULL,
+    PRIMARY KEY (carparkID)
+);
+
+CREATE Table "ParkingSpace" (
+    parkingSpaceID INTEGER,
+    carparkID INTEGER NOT NULL,
+    gps POINT NOT NULL,
+    occupied  BOOLEAN DEFAULT false,
+    registration VARCHAR(7),
+    PRIMARY KEY (parkingSpaceID),
+    FOREIGN KEY (carparkID) REFERENCES "Carpark" (carparkID),
+    FOREIGN KEY (registration) REFERENCES "Vehicle" (registration)
+);
---
+++ database/scripts/views/create_views.sql
@@ -0,0 +1,2 @@
+CREATE VIEW Admin AS
+    SELECT
