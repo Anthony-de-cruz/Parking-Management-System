ce2146a97ca5d990086155e61a9fbb9e92ff583f
Anthony de Cruz <anthonydecruz@proton.me>
2024-04-22 11:16:47 +0100

    Login feature

--- node/app.js
+++ node/app.js
@@ -10,6 +10,7 @@ const { query } = databaseManager;
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+var loginRouter = require("./routes/login");
 
 var app = express();
 
@@ -35,6 +36,7 @@ pokeDB();
 // routing
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/login", loginRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
