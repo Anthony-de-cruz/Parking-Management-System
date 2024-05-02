1964ac4d42c2d5ce635b37d84de6c8ce4f26620d
Anthony de Cruz <anthonydecruz@proton.me>
2024-05-02 16:42:24 +0100

    Updated user and booking for consistency

--- node/models/booking.js
+++ node/models/booking.js
@@ -1,9 +1,28 @@
-class Booking{
-    constructor(spaceID, carparkID, gps, blocked, occupantName){
-        this.spaceID = spaceID
-        this.carparkID = carparkID;
-        this.gps = gps;
-        this.blocked = blocked;
-        this.occupantName = occupantName;
-    }
+/**
+ * Represents bookings made for the
+ */
+class Booking {
+  /**
+   * @param {number} bookingID
+   * @param {number} parkingSpaceID
+   * @param {string} bookingUsername
+   * @param {Date} start
+   * @param {Date} finish
+   * @param {number} deposit
+   */
+  constructor(
+    bookingID,
+    parkingSpaceID,
+    bookingUsername,
+    start,
+    finish,
+    deposit,
+  ) {
+    this.bookingID = bookingID;
+    this.parkingSpaceID = parkingSpaceID;
+    this.bookingUsername = bookingUsername;
+    this.start = start;
+    this.finish = finish;
+    this.deposit = deposit;
+  }
 }
