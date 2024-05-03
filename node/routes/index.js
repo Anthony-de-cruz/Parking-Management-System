dbc5154c11eca274a40ff3b7e21f189cc2d9fe12
Anthony de Cruz <anthonydecruz@proton.me>
2024-05-03 19:05:40 +0100

    Made a start on conditional rendering

--- node/routes/index.js
+++ node/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "CarpPark" });
+  res.render("index", { loggedIn: false, balance: 0 });
 });
 
 module.exports = router;
