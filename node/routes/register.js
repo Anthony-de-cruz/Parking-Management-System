e78098177fd50f16650ae85416e44b0025168b4f
Anthony de Cruz <anthonydecruz@proton.me>
2024-05-08 19:41:12 +0100

    Implemented registration functionality

--- node/routes/register.js
+++ node/routes/register.js
@@ -8,6 +8,24 @@ router.get("/", function (req, res, next) {
   res.render("register", {});
 });
 
-router.post("/", function (req, res, next) {});
+router.post("/", async (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  const email = req.body.email;
+
+  console.log(
+    "Attempted register as: " + username + "," + password + "," + email,
+  );
+
+  try {
+    await LoginRegisterController.registerUser(username, password, email);
+    return res.redirect("/login");
+  } catch (error) {
+    console.log("ERR: Registration failed");
+    return res.render("register", {
+      registerResult: "ERR: Registration failed",
+    });
+  }
+});
 
 module.exports = router;
