262f413fb178cd2edb9e3e4d6a9ff43da2b051e6
Anthony de Cruz <anthonydecruz@proton.me>
2024-05-11 04:01:19 +0100

    Made booking page able to book from desired location

--- node/routes/createBooking.js
+++ node/routes/createBooking.js
@@ -10,12 +10,31 @@ router.get(
   LoginRegisterController.checkAuthToken,
   LoginRegisterController.collectAuthTokenData,
   async (req, res, next) => {
-    res.render("createBooking", { loggedIn: req.loggedIn, user: req.user });
+    res.render("createBooking", {
+      loggedIn: req.loggedIn,
+      user: req.user,
+      calculatedBooking: req.calculatedBooking,
+      newBooking: req.newBooking,
+    });
   },
 );
 
 router.post(
   "/",
+  LoginRegisterController.collectAuthTokenData,
+  UserController.calcualteBooking,
+  async (req, res, next) => {
+    res.render("createBooking", {
+      loggedIn: req.loggedIn,
+      user: req.user,
+      calculatedBooking: req.calculatedBooking,
+      newBooking: req.newBooking,
+    });
+  },
+);
+
+router.post(
+  "/book",
   LoginRegisterController.collectAuthTokenData,
   UserController.createBooking,
 );
