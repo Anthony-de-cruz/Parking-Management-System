4930cd64df3b52a65d1da7897111b97187e90af7
astrallax <r.abraham0311@gmail.com>
2024-05-14 02:35:08 +0100

    Added adminManageParking functionality
    Added functionality for adminManageParking

--- node/routes/adminManageParking.js
+++ node/routes/adminManageParking.js
@@ -2,32 +2,36 @@ var express = require("express");
 var router = express.Router();
 
 const LoginRegisterController = require("../controllers/loginRegisterController");
-const User = require("../models/user");
+const AdminController = require("../controllers/adminController");
 
-
-
-/* GET create booking page. */
+/* GET manage parking page. */
 router.get(
   "/",
   LoginRegisterController.checkAuthToken,
   LoginRegisterController.collectAuthTokenData,
+  AdminController.getParkingBookings,
   async function (req, res, next) {
-    res.render("adminManageParking", { loggedIn: req.loggedIn, user: req.user });
-  },
-);
-
-router.post(
-  "/update",
-  LoginRegisterController.checkAuthToken,
-  LoginRegisterController.collectAuthTokenData,
-  async function (req, res, next) {
-    
-      // Redirect back to the manageAccount page with a success message
-      res.redirect('/admin-manage-parking?success=true');
-
+    res.render("adminManageParking", { loggedIn: req.loggedIn, user: req.user, bookings: req.bookings });
   }
 );
 
+router.post(
+  "/remove",
+  LoginRegisterController.checkAuthToken,
+  AdminController.removeBooking
+);
+
+router.post(
+  "/toggle-block",
+  LoginRegisterController.checkAuthToken,
+  AdminController.toggleBlock
+);
+
+router.post(
+  "/reserve",
+  LoginRegisterController.checkAuthToken,
+  AdminController.reserve
+);
 
 module.exports = router;
 
