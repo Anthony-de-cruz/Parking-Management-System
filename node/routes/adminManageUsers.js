dc2ac18930d6ed51850bfc349ad1cb5035d8d1a3
astrallax <r.abraham0311@gmail.com>
2024-05-14 03:23:24 +0100

    Added adminManageUsers functionality
    Added adminManageUsers functionality

--- node/routes/adminManageUsers.js
+++ node/routes/adminManageUsers.js
@@ -2,22 +2,22 @@ var express = require("express");
 var router = express.Router();
 
 const LoginRegisterController = require("../controllers/loginRegisterController");
-const User = require("../models/user");
+const AdminController = require("../controllers/adminController");
 
-/* GET create booking page. */
+/* GET manage users page. */
 router.get(
   "/",
   LoginRegisterController.checkAuthToken,
   LoginRegisterController.collectAuthTokenData,
   async function (req, res, next) {
     try {
-      const users = await User.getUsers();
+      const users = await AdminController.getUsers();
       res.render("adminManageUsers", { loggedIn: req.loggedIn, user: req.user, users: users });
     } catch (error) {
       console.error("Failed to fetch users:", error);
       res.status(500).render('adminManageUsers', { loggedIn: req.loggedIn, user: req.user, error: "Failed to fetch users." });
     }
-  },
+  }
 );
 
 router.post(
@@ -25,48 +25,32 @@ router.post(
   LoginRegisterController.checkAuthToken,
   LoginRegisterController.collectAuthTokenData,
   async function (req, res, next) {
-    const { action, username} = req.body;
-    const isAdmin = req.user.isAdmin; 
+    const { action, username } = req.body;
+    const isAdmin = req.user.isAdmin;
 
     try {
-
       if (!isAdmin) {
         throw new Error("Only admins can perform this action.");
       }
 
       if (action === "ban") {
-        console.log("action ban works");
-        await banUser(username);
-        console.log("function ban works");
-      //} else if (action === "unban") {
-      //  await User.unbanUser(username, isAdmin);
+        await AdminController.banUser(username);
+      } else if (action === "unban") {
+        await AdminController.unbanUser(username);
       } else if (action === "delete") {
-        console.log("action delete works");
-        await deleteUser(username);
-        console.log("function delete works");
+        await AdminController.deleteUser(username);
       } else {
         throw new Error("Invalid action.");
       }
 
-      // Redirect back to the manageAccount page with a success message
-      res.redirect('/manage-users?success=true');
+      res.redirect('/admin-manage-users');
     } catch (error) {
-      console.error("Failed to update account:", error);
-      res.status(500).render('adminManageUsers', { loggedIn: req.loggedIn, user: req.user, error: "Failed to update account due to server error." });
+      console.error("Failed to update user:", error);
+      res.status(500).render('adminManageUsers', { loggedIn: req.loggedIn, user: req.user, error: "Failed to update user due to server error." });
     }
   }
 );
 
-async function deleteUser(button) {
-  var username = button.dataset.username;
-  await query("DELETE FROM app_user WHERE username = $1;", [username]);
-  // Perform delete user action
-}
-
-async function banUser(button) {
-  var username = button.dataset.username;
-  // Perform ban user action
-}
 
 
 module.exports = router;
