2cdc25a7ef2fa89b40de7fa082a103c36a7ed635
John <92218250+John-W-Callaghan@users.noreply.github.com>
2024-05-16 13:27:13 +0100

    Register comms and cleanup

--- node/routes/register.js
+++ node/routes/register.js
@@ -2,11 +2,13 @@ var express = require("express");
 var router = express.Router();
 
 const LoginRegisterController = require("../controllers/loginRegisterController");
+const { sendEmail } = require("../controllers/emailService"); 
 
 /* GET create register page. */
 router.get("/", function (req, res, next) {
   res.render("register", {});
 });
+
 
 router.post("/", async (req, res, next) => {
   const username = req.body.username;
@@ -19,10 +21,17 @@ router.post("/", async (req, res, next) => {
 
   try {
     await LoginRegisterController.registerUser(username, password, email);
+    
+    // Send the email notification
+    const subject = "Account Created Successfully";
+    const text = `Hello ${username},\n\nYour account has been created successfully. Here are your details:\n\nUsername: ${username}\nEmail: ${email}\n\nThank you for registering!\n\nBest regards,\nCarppark`;
+
+    await sendEmail(email, subject, text);
+
     return res.redirect("/login");
   } catch (error) {
     // Not a particularly understandable error message
-    console.log("ERR: Registration failed");
+    console.log("ERR: Registration failed", error);
     return res.render("register", {
       registerResult: "ERR: Registration failed",
     });
