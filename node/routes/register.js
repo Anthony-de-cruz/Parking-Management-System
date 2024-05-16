9e1af2179a1c04b88be0e8276d018f74f4bd2c7f
Anthony de Cruz <anthonydecruz@proton.me>
2024-05-16 16:00:10 +0100

    Fixed registration bug

--- node/routes/register.js
+++ node/routes/register.js
@@ -2,26 +2,31 @@ var express = require("express");
 var router = express.Router();
 
 const LoginRegisterController = require("../controllers/loginRegisterController");
-const { sendEmail } = require("../controllers/emailService"); 
+const { sendEmail } = require("../controllers/emailService");
 
 /* GET create register page. */
 router.get("/", function (req, res, next) {
   res.render("register", {});
 });
 
-
 router.post("/", async (req, res, next) => {
   const username = req.body.username;
   const password = req.body.password;
   const email = req.body.email;
+  const phone_number = req.body.phone_number;
 
   console.log(
     "Attempted register as: " + username + "," + password + "," + email,
   );
 
   try {
-    await LoginRegisterController.registerUser(username, password, email);
-    
+    await LoginRegisterController.registerUser(
+      username,
+      password,
+      email,
+      phone_number,
+    );
+
     // Send the email notification
     const subject = "Account Created Successfully";
     const text = `Hello ${username},\n\nYour account has been created successfully. Here are your details:\n\nUsername: ${username}\nEmail: ${email}\n\nThank you for registering!\n\nBest regards,\nCarppark`;
